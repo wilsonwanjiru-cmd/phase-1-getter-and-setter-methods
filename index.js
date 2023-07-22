eee8fa686ff8f0d6cb6d19191e6729584d2b4488
Wilson Muita Wanjiru <wilsonmuita41@gmail.com>
2023-07-22 17:52:27 +0300

    <Getter and Setter Methods>

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your Circle class here
+// index.js
+
+class Circle {
+    constructor(radius) {
+      this.radius = radius;
+    }
+  
+    // Define getter methods for diameter, circumference, and area
+    get diameter() {
+      return this.radius * 2;
+    }
+  
+    get circumference() {
+      return Math.PI * this.diameter;
+    }
+  
+    get area() {
+      return Math.PI * Math.pow(this.radius, 2);
+    }
+  
+    // Define setter methods for diameter, circumference, and area
+    set diameter(diameter) {
+      this.radius = diameter / 2;
+    }
+  
+    set circumference(circumference) {
+      this.radius = circumference / (2 * Math.PI);
+    }
+  
+    set area(area) {
+      this.radius = Math.sqrt(area / Math.PI);
+    }
+  }
+  
+  // Export the Circle class
+  module.exports = Circle;
+  
